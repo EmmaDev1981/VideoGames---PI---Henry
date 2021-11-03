95aee961ffac120f59271254037fa1b50099bc07
Emmanuel <emanuelnavas@hotmail.com>
2021-11-03 12:24:00 -0300

    update deploy

--- api/index.js
+++ api/index.js
@@ -24,7 +24,7 @@ const { conn } = require('./src/db.js');
 
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
-  server.listen(3001, () => {
+  server.listen(process.env.PORT, () => {
     console.log('\x1b[33m%s\x1b[0m', 'server listening at 3001'); // eslint-disable-line no-console
   });
 });
