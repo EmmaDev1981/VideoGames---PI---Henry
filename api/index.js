6cd0b8cb653a1ae94c1caed2b794721b9be015a9
Emmanuel <emanuelnavas@hotmail.com>
2021-11-09 18:18:17 -0300

    update

--- api/index.js
+++ api/index.js
@@ -21,8 +21,22 @@
 // Syncing all the models at once.
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
+const axios = require('axios');
+const dot = require('dotenv')
+
+dot.config()
+axios.defaults.baseURL = 'http://localhost:3001';
+
+var local = "";
+
+if(process.env.PORT == 3001){
+  local = "http://localhost:3001";
+}else{
+  local = "https://videogames-pi-henry.herokuapp.com/";
+}
 
 // Syncing all the models at once.
+console.log(process.env.PORT)
 conn.sync({ force: true }).then(() => {
   server.listen(process.env.PORT, () => {
     console.log('\x1b[33m%s\x1b[0m', 'server listening at 3001'); // eslint-disable-line no-console
