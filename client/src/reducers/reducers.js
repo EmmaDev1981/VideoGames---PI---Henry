d392fb08cc0f136a64f19a63e46b278fdaad94cd
Emmanuel Navas <82782857+EmmaDev1981@users.noreply.github.com>
2021-11-30 07:00:46 -0300

    Update reducers.js

--- client/src/reducers/reducers.js
+++ client/src/reducers/reducers.js
@@ -48,12 +48,15 @@ export default function rootReducer(state = initialState, action) {
                     if (action.payload === 'default'){
                         return {...state, filtered: state.gamesBackUp}
                         }
+                      
                     if(action.payload === 'DB'){
                         return {...state, filtered: state.gamesBackUp.filter((game)=> (typeof game.id) === 'string')}
                         }
+                      
                     if(action.payload === 'API'){
                         return {...state, filtered: state.gamesBackUp.filter((game)=> (typeof game.id) === 'number')}
                         }
+                      
                     else {
                         return {...state, filtered: state.gamesBackUp.filter((game) => {
                             return game.genres.find((genre) => {
@@ -68,18 +71,22 @@ export default function rootReducer(state = initialState, action) {
                         if(prev.name < next.name) return -1
                         return 0
                     })}}
+                      
                 if(action.payload === 'Z-A'){
                     return {...state, filtered: [...state.filtered].sort((prev, next) => {
                         if(prev.name > next.name) return -1
                         if(prev.name < next.name) return 1
                         return 0
                     })}}
+                      
                 if(action.payload === 'desc'){
                     return {...state, filtered: [...state.filtered].sort((prev,next) => prev.rating - next.rating)}
                    }
+                      
                 if(action.payload === 'asc'){
                     return {...state, filtered: [...state.filtered].sort((prev,next) => next.rating - prev.rating)}
                     }
+                      
                 else {
                     return {...state, filtered: state.gamesBackUp}
                     };
