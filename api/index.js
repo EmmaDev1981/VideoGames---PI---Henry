5e69a6dc505336582db1725edc03e356cd11dfc6
Emmanuel <emanuelnavas@hotmail.com>
2023-07-10 15:33:36 -0300

    fix url

--- api/index.js
+++ api/index.js
@@ -32,7 +32,7 @@ var local = "";
 if(process.env.PORT == 3001){
   local = "http://localhost:3001";
 }else{
-  local = "https://videogames-pi-henry.herokuapp.com";
+  local = "https://video-games-pi-henry-api.vercel.app/";
 }
 
 // Syncing all the models at once.
